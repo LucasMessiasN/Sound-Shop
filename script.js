ab41d9a62f0c4bfb3f623c8b7a6c7446e18cefd0
Lucas Messias <159068439+LucasMessiasN@users.noreply.github.com>
2025-03-10 15:44:21 -0400

    Loja de Musica
    To começando, não sei javascript de fato, então fiz o que deu so pra ficar "bonitin"

---
+++ script.js
@@ -0,0 +1,11 @@
+//window.alert('Welcome') //Cria uma mensagem de alerta'
+//window.confirm('You want to acess the website') //Cria um alerta com o botão de "Ok" e "Cancelar"
+//window.prompt('lorem') - Cria uma caixa de texto
+
+//document.addEventListener("DOMContentLoaded", function() {
+    //var nome = window.prompt('What´s Your Name?');
+    //document.getElementById("resultado").innerHTML = "Olá, " + nome + "! Aproveite nosso site.";});
+
+    function comprar() {
+        alert("Produto adicionado ao carrinho!");
+    }
